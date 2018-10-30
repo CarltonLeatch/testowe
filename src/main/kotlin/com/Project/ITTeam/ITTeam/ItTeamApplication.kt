c32e35f6c16b1800fcb8f7cb4a4b946081d61d75
Rafal <carltonleatch@gmail.com>
2018-10-30 20:03:08 +0100

    init

---
+++ src/main/kotlin/com/Project/ITTeam/ITTeam/ItTeamApplication.kt
@@ -0,0 +1,11 @@
+package com.Project.ITTeam.ITTeam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItTeamApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItTeamApplication>(*args)
+}
